aeb00a7071a7100bb931002131326ffbc793068b
b-viv <134312026+b-viv@users.noreply.github.com>
2023-10-17 16:09:03 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const openMenu = document.querySelector(".menu-open");
+const closeMenu = document.querySelector(".menu-close");
+const header = document.querySelector("header");
+const items = document.querySelectorAll("nav a");
+
+openMenu.addEventListener("click",function show(){
+    header.style.height = "22rem";
+    openMenu.style.display = "none";
+    closeMenu.style.display = "inline-block";
+});
+closeMenu.addEventListener("click",function hide(){
+    header.style.height = "5rem";
+    openMenu.style.display = "inline-block";
+    closeMenu.style.display = "none";
+
+});
+
