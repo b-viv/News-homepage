813403a9c15b4016939261bb2382a18f8fdbe97d
b-vivien <134312026+b-viv@users.noreply.github.com>
2023-10-19 10:44:49 +0200

    updated

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const openMenu = document.querySelector(".menu-open");
 const closeMenu = document.querySelector(".menu-close");
 const header = document.querySelector("header");
-const items = document.querySelectorAll("nav a");
 
 openMenu.addEventListener("click",function show(){
     header.style.height = "22rem";
     openMenu.style.display = "none";
     closeMenu.style.display = "inline-block";
+
 });
 closeMenu.addEventListener("click",function hide(){
     header.style.height = "5rem";
@@ -14,4 +14,5 @@ closeMenu.addEventListener("click",function hide(){
     closeMenu.style.display = "none";
 
 });
+
 
